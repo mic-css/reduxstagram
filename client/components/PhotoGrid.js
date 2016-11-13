9b288b040fde5f5e3038d9f13df9cedd85fec605
Mic Cassano <mic.cassano@gmail.com>
2016-11-13 19:42:28 +0000

    Add boilerplate reducers and App component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -4,7 +4,9 @@ class PhotoGrid extends React.Component {
   render() {
     return (
       <div className="photo-grid">
-        I'm the photo grid
+        <pre>
+          {JSON.stringify(this.props.posts, null, ' ')}
+        </pre>
       </div>
     );
   }
